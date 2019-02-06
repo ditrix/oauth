f1da5c29bc6d4129efe9120c9ea6b6f538cf174a
dirix <ditrix2006@gmail.com>
2019-02-06 15:23:50 +0200

    fb login/logout simple

--- src/components/FacebookOAuth.js
+++ src/components/FacebookOAuth.js
@@ -5,11 +5,9 @@ export default class FacebookOAuth extends Component {
 	constructor(){
 		super()
     this.state = {
-      signed: false,
+      connected: false,
       id: null,
       name: null,
-      email: null,
-      imageUrl: null,
     }
     this.handleLogin = this.handleLogin.bind(this)
     this.handleLogout = this.handleLogout.bind(this)
@@ -19,92 +17,33 @@ export default class FacebookOAuth extends Component {
    statusChangeCallback(response){
       window.FB.api('/me', function(response) {
           console.log('statusChangeCallback',response)
-          this.setState({name:response.name, id: response.id, signed: (response.id !== undefined) })
-          console.log('this.state.after',this.state);
+          this.setState({
+            name:response.name, 
+            id: response.id, 
+            connected: (response.id !== undefined) 
+          })
       }.bind(this))
    }
 
-
-  checkLoginState() {
-    window.FB.getLoginStatus(function(response) {
-      this.statusChangeCallback(response);
-      console.log('checkLoginState-------')
-      console.log(response)
-      console.log('-------checkLoginState')
-    }.bind(this));
-  }
-
-// https://developers.facebook.com/docs/javascript/examples/#login
-// https://developers.facebook.com/docs/reference/javascript/FB.getLoginStatus/
-// https://stackoverflow.com/questions/27717555/implement-facebook-api-login-with-reactjs/31859302
 handleLogin() {
-  if(!this.state.signed) {
-    window.FB.login(function(response) {
-        if(response.authResponse){
-          window.FB.api('/me',function(reponse){
-            console.log(response)
-           this.setState({signed: true, name: response.name})
-          }.bind(this)) // .api('/me',..)
-        }
-      }.bind(this)) // .login
-  }
- /* window.FB.getLoginStatus(function(response){
-    if(response !== 'connected'){
-       window.FB.login(function(response) {
-        if(response.authResponse){
-          window.FB.api('/me',function(reponse){
-            console.log(response)
-           this.setState({signed: true, name: response.name})
-          }.bind(this)) // .api('/me',..)
-        }
-      }.bind(this)) // .login
-      }    
-  }.bind(this)) // .getLoginStatus
-  console.log(this.state)
-}  */
-/*
-  window.FB.login(function(response) {
-    if (response.authResponse) {
-     console.log('Welcome!  Fetching your information.... ');
-     window.FB.api('/me', function(response) {
-       console.log('Good to see you, ' + response.name + '.');
-     });
-    } else {
-     console.log('User cancelled login or did not fully authorize.');
-    }
-});
-}
-*/
-/*
-
-handleLogin() {
-
-  window.FB.getLoginStatus(function(response) {
-  // this will be called when the roundtrip to Facebook has completed
-    console.log('handleLogin=>',response)
-    this.setState({signed: (response.status == 'connected')?true : false })
-    //console.log(this.state)
-  }.bind(this), true);
-
-  if(!this.state.signed){
     window.FB.login(
       function(response){
-        if (response.authResponse) {
-          console.log('Welcome!  Fetching your information.... ');
-          window.FB.api('/me', function(response) {
-              console.log('Good to see you, ' + response.name + '.');
-              this.setState({signed: true, name:response.name})    
-          }.bind(this))
-      }
-    }.bind(this)//function(response)
+         this.statusChangeCallback(response);   
+      }.bind(this) // FB.api ...
     )
-  }
-  console.log(this.state)
-*/ 
+    //console.log(this.state)
 }
 
+
+
+
 handleLogout() {
-  window.FB.logout(this.checkLoginState());
+    window.FB.logout(
+      function(response){
+         this.statusChangeCallback(response);   
+      }.bind(this) // FB.api ...
+    )
+    //console.log(this.state)
  }
 
 
@@ -122,8 +61,8 @@ handleLogout() {
       window.FB.getLoginStatus(
         function(response){
           this.statusChangeCallback(response);
-      }.bind(this))
-    }.bind(this);
+        }.bind(this))
+      }.bind(this);
 
     // Load the SDK asynchronously
     (function(d, s, id) {
@@ -147,6 +86,7 @@ handleLogout() {
       <button onClick={this.handleLogout}>logout</button>
       <br />
       <div id="status">{this.state.name}</div>
+      
       </section>
 		)
 	}
@@ -157,3 +97,6 @@ handleLogout() {
 // https://developers.facebook.com/docs/facebook-login/web/#example
 // https://developers.facebook.com/docs/graph-api/reference/v2.1/user
 // https://stackoverflow.com/questions/27717555/implement-facebook-api-login-with-reactjs
+// https://developers.facebook.com/docs/javascript/examples/#login
+// https://developers.facebook.com/docs/reference/javascript/FB.getLoginStatus/
+// https://stackoverflow.com/questions/27717555/implement-facebook-api-login-with-reactjs/31859302
